ea201dc8f61e3b6f699cc6c70ad55753a120f7dc
InsaneXo <vsinghvipin333@gmail.com>
2025-02-23 13:18:52 +0530

    Add something new

--- array_deletion.js
+++ array_deletion.js
@@ -3,9 +3,11 @@ const data = [10, 20, 30, 40, 50, 60];
 const arrayDeletion = (position) => {
   for (let i = position; i < data.length - 1; i++) {
     data[i] = data[i + 1];
+    console.log("data[i] = data[i + 1];", data[i] = data[i + 1])
+    console.log("Array is : ", data)
   }
   data.length = data.length - 1;
   console.log(data);
 };
 
-arrayDeletion(4);
+arrayDeletion(2);
